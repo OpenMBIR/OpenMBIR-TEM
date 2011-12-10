c6347e37fb4ebd1bcd14364532c4bff09d382e22
Singanallur Venkatakrishnan <svenkata@purdue.edu>
2011-12-09 19:44:33 -0500

    Just changing some flags

--- Code/TomoEngine/SOC/SOCConstants.h
+++ Code/TomoEngine/SOC/SOCConstants.h
@@ -42,6 +42,8 @@
 #include <string>
 
 #define EXTEND_OBJECT
+#define X_SHRINK_FACTOR 0.6
+#define TARGET_GAIN 1
 
 #define X_STRETCH 1
 #define Z_STRETCH 2
@@ -58,7 +60,7 @@
 #define ROI 1
 
 #define STOPPING_THRESHOLD 0.009
-#define THRESHOLD_REDUCTION_FACTOR .5 //Dynamically lower the threshold by this amount. Set to 1 for no reduction
+#define THRESHOLD_REDUCTION_FACTOR .1 //Dynamically lower the threshold by this amount. Set to 1 for no reduction
 //#define SURROGATE_FUNCTION
 //#define QGGMRF
 //#define DISTANCE_DRIVEN
@@ -69,15 +71,14 @@
 #define DETECTOR_RESPONSE_BINS 64
 #define JOINT_ESTIMATION
 //#define GEOMETRIC_MEAN_CONSTRAINT
-#define NOISE_MODEL
+//#define NOISE_MODEL
 #define POSITIVITY_CONSTRAINT
 //#define CIRCULAR_BOUNDARY_CONDITION
 //#define DEBUG_CONSTRAINT_OPT
 #define RANDOM_ORDER_UPDATES
 //#define BRIGHT_FIELD
 //#define NHICD
-#define X_SHRINK_FACTOR 0.6
-#define TARGET_GAIN 1
+
 
 //#define FORWARD_PROJECT_MODE //this Flag just takes the input file , forward projects it and exits
 
