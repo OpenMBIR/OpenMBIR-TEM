63c6fecda70484711eb5befda4aca7465b1df67a
Singanallur Venkatakrishnan <svenkata@purdue.edu>
2012-09-12 13:07:24 -0400

    Making some small changes to test the waters for using this for Bright Field only tomography

--- Code/MBIRLib/Reconstruction/ReconstructionConstants.h
+++ Code/MBIRLib/Reconstruction/ReconstructionConstants.h
@@ -66,7 +66,7 @@ namespace VoxelUpdateType
 
 //#define WRITE_INTERMEDIATE_RESULTS
 //#define COST_CALCULATE
-//#define BF_RECON //Bright Field Reconstruction
+#define BF_RECON //Bright Field Reconstruction
 #define BF_OFFSET 0//0 //23696 - Bio data set
 #define BF_MAX  1865//5689//42122
 
