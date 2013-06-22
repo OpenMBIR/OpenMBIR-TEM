88510361a6cc211cc48e36b8658e0e78438c6a53
Singanallur Venkatakrishnan <svenkata@purdue.edu>
2013-06-22 18:21:01 -0400

    Commit all changes. Going to start major coding changes

--- Code/MBIRLib/Reconstruction/ReconstructionConstants.h
+++ Code/MBIRLib/Reconstruction/ReconstructionConstants.h
@@ -71,7 +71,7 @@ namespace VoxelUpdateType
 //#define REJECTION_PERCENTAGE 0.1 //fraction of singoram affected by Bragg
 #define REJECTION_RATE 10 //Number of steps over which to increase the Bragg threshold to its desired value
 #define BF_RECON //Bright Field Reconstruction
-#define BF_OFFSET 32768//0//23696 //- Bio data set
+#define BF_OFFSET 0//32768//0//23696 //- Bio data set
 #define BF_MAX  1//1865//30369//5689//42122//1865//32768//
 //#define IDENTITY_NOISE_MODEL
 #define POSITIVITY_CONSTRAINT
